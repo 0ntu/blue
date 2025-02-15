ac1d43601eff9e2e2177bb49c7e1a9287c6773b1
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-02-15 14:19:01 -0500

    Fix package install script

--- ccdc_2025/setup/isp/packageinstall.sh
+++ ccdc_2025/setup/isp/packageinstall.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install postgres postgresql-contrib caddy nodejs npm -y
+apt-get install postgresql postgresql-contrib caddy nodejs npm -y
 
 wget https://github.com/binwiederhier/ntfy/releases/download/v2.11.0/ntfy_2.11.0_linux_amd64.deb
 dpkg -i ntfy_*.deb
