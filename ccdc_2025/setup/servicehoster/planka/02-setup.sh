f81e0cbdd8d8c1656f285207f800e768c48c0e68
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-02-15 14:08:22 -0500

    Remove unnecessary repos
    Speeds up repo clone time by lots

--- ccdc_2025/setup/servicehoster/planka/02-setup.sh
+++
@@ -1,7 +0,0 @@
-psql -c "ALTER USER planka PASSWORD 'DB_PASSWORD_CHANGE_ME';"
-cd /var/www/planka
-curl -fsSL -O https://github.com/plankanban/planka/releases/latest/download/planka-prebuild.zip
-unzip planka-prebuild.zip -d /var/www/
-rm planka-prebuild.zip
-cd planka
-npm install
