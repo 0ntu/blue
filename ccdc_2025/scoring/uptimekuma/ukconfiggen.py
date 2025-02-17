bc5eff873c225a34d6814706c25074c5620ad11e
yhuang3-uf <115114652+yhuang3-uf@users.noreply.github.com>
2025-02-17 15:17:49 -0500

    Fix bug in script

--- ccdc_2025/scoring/uptimekuma/ukconfiggen.py
+++ ccdc_2025/scoring/uptimekuma/ukconfiggen.py
@@ -18,38 +18,68 @@ Except as contained in this notice, the name of the copyright holder shall not b
 ```
 """
 
+import argparse
 import csv
 import json
+import pathlib
 import sys
 import typing
 
 def main(argv: list[str]) -> int:
+    argparser: argparse.ArgumentParser = argparse.ArgumentParser()
+    argparser.add_argument("input_services_file", type=pathlib.Path,
+            help="A CSV file containing service information")
+    argparser.add_argument("output_json_file", type=pathlib.Path,
+            help="The JSON file that should be outputted")
+    parsedargs: dict[str, typing.Any] = vars(argparser.parse_args())
+
     monitor_list: list[dict] = []
     """The list of services to monitor"""
+
+    hosts_list: dict[str, str] = {}
+    """The list of hosts to monitor. Maps ip_address to hostname"""
+    ports_list: list[tuple[str,str,typing.Optional[int]]] = set()
+    """
+    The host/port combinations to monitor (description, ip, port)
+    port will be None for ICMP
+    """
     with open("services.csv") as inputcsvfile:
         inputcsvreader: csv.DictReader = csv.DictReader(inputcsvfile)
         for i, row in enumerate(inputcsvreader):
             port_number: typing.Optional[int] = None
-            is_icmp: bool = False
             try:
-                if row["port"].strip().lower() in ("ping", "icmp"):
-                    is_icmp = True
                 port_number = int(row["port"])
             except ValueError:
                 pass
-            if port_number is None and not is_icmp:
+            if port_number is None:
                 # Failed to parse port number
                 continue
+            description: str = ""
+            if row["hostname"] is None or row["hostname"].strip() == "":
+                description += row["ip"]
+            else:
+                description += row["hostname"]
+            description += " " + row["service"]
+            ports_list.append((row["ip"], port_number))
+            hosts_list[row["ip"]] = row["hostname"]
+
+    for ip_address in hosts_list:
+        if hosts_list[ip_address].strip() is None:
+            ports_list.append(ip_address + " ICMP", ip_address, None)
+        else:
+            ports_list.append(hosts_list[ip_address] + " ICMP", ip_address, None)
+
+    for description, ip_address, port_number in ports_list:
             monitor_list.append({
-                "id": i+1, "name": row["description"], "description": None,
-                "pathName": row["description"], "parent": None,
+                "id": i+1, "name": description, "description": None,
+                "pathName": description, "parent": None,
                 "childrenIDs": [], "url": "https://", "method": "GET",
-                "hostname": row["host"], "port": port_number, "maxretries": 0,
+                "hostname": ip_address, "port": port_number, "maxretries": 0,
                 "weight": 2000, "active": True, "forceInactive": False, 
-                "type": ("ping" if is_icmp else "port"), "timeout": 48,
+                "type": ("ping" if port_number is None else "port"), "timeout": 48,
                 "interval": 60, "retryInterval": 60, "resendInterval": 0,
                 "keyword": None, "invertKeyword": False, 
-                "expiryNotification": False, "ignoreTls": False, 
+                "expiryNotification": False, "ignoreTls": True, 
                 "upsideDown": False, "packetSize": 56, "maxredirects": 10,
                 "accepted_statuscodes": ["200-299"], "dns_resolve_type": "A",
                 "dns_resolve_server": "1.1.1.1", "dns_last_result": None,
--- ccdc_2025/setup/isp/planka/02-setup.sh
+++ ccdc_2025/setup/isp/planka/02-setup.sh
@@ -1,7 +1,8 @@
 psql -c "ALTER USER planka PASSWORD 'DB_PASSWORD_CHANGE_ME';"
-cd /var/www/planka
+cd /tmp
 curl -fsSL -O https://github.com/plankanban/planka/releases/latest/download/planka-prebuild.zip
 unzip planka-prebuild.zip -d /var/www/
+cd /var/www/planka
 rm planka-prebuild.zip
 cd planka
 npm install
